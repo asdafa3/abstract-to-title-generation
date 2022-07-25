c5fcf25cd4a3227435535bed506e689ee5a6f5c2
Linus Schwarz <linus.schwarz@uni-bielefeld.de>
2022-07-25 17:44:17 +0200

    Changed config.py and adapted imports and path in RL_Optimization.ipynb

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import torch
 import subprocess
 import os
 
+from zmq import device
+
 def mount_root():
     subprocess.run(["pip", "install", "git_root"])
 
@@ -20,7 +22,7 @@ def mount_root():
       PROJECT_ROOT = git_root()
     return PROJECT_ROOT
 
-def get_device():
+def get_device_string():
     if torch.cuda.is_available():
         device_id = "cuda:0"
     elif torch.backends.mps.is_available():
@@ -30,9 +32,15 @@ def get_device():
 
     print(device_id)
 
+    return device_id
+    
+
+def get_torch_device():
+    device_id = get_device_string()
     return torch.device(device_id)
 
 PROJECT_ROOT = mount_root()
+DEVICE_ID = get_device_string()
 
 os.chdir(PROJECT_ROOT)
 
@@ -40,6 +48,7 @@ DATA_DIR = f"{PROJECT_ROOT}/data"
 OUTPUT_DIR = f"{PROJECT_ROOT}/output"
 
 MODEL_DIR = f"{PROJECT_ROOT}/model"
+RL = f"{PROJECT_ROOT}/RL_bart_xsum"
 
 DATASET_140_ANNOTATED_JSON = f'{OUTPUT_DIR}/annotated_json/dataset_140samples.json'
 
